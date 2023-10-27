e9ae2e4bea79702cbc661d7b310a1ede506bd933
Delowar-maker <56905688+Delowar-maker@users.noreply.github.com>
2023-10-28 02:52:16 +0600

    form Data
    form data get

--- src/app/api/product/route.js
+++ src/app/api/product/route.js
@@ -19,7 +19,31 @@ import { NextResponse } from "next/server";
 
 // }
 
+// req.body 
+// export async function POST(req, res){
+//     const reqBody = await req.json()
+//     return NextResponse.json({mass:reqBody})
+// }
+
+//form Data req Body 
+
+
+// export async function POST(req, res){
+//     const formData = await req.json()
+//     return NextResponse.json({mass:formData})
+// }
+
+//form data get
+//formData key value pairs
 export async function POST(req, res){
-    const reqBody = await req.json()
-    return NextResponse.json({mass:reqBody.name})
+    const formData = await req.formData();
+    let mobile = formData.get('mobile')
+    let postcode = formData.get('postcode')
+    let city = formData.get('city')
+    return NextResponse.json({
+        // value set 
+        mobile:mobile,
+        postcode: postcode,
+        city:city
+    })
 }
