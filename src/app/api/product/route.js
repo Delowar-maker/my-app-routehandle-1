2d42188dad17a0a70b7bab7b8c874fb8d22e9d03
Delowar-maker <56905688+Delowar-maker@users.noreply.github.com>
2023-10-28 02:35:17 +0600

    req.body
    get data body

--- src/app/api/product/route.js
+++ src/app/api/product/route.js
@@ -8,13 +8,18 @@ import { NextResponse } from "next/server";
 
 // }
 
-export function GET(req, res){
-    // const {searchParams} = new URL(req.url);
-    // const id = searchParams.get('id')
-    // return NextResponse.json({msg:id});
-    const {searchParams} = new URL(req.url);
-    const name = searchParams.get('name')
-    const city = searchParams.get('city')
-    return NextResponse.json({name, city});
+// export function GET(req, res){
+//     // const {searchParams} = new URL(req.url);
+//     // const id = searchParams.get('id')
+//     // return NextResponse.json({msg:id});
+//     const {searchParams} = new URL(req.url);
+//     const name = searchParams.get('name')
+//     const city = searchParams.get('city')
+//     return NextResponse.json({name, city});
 
+// }
+
+export async function POST(req, res){
+    const reqBody = await req.json()
+    return NextResponse.json({mass:reqBody.name})
 }
