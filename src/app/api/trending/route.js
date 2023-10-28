1949374821cc5ee071b3eb722ca8ced2c5994c9e
Delowar-maker <56905688+Delowar-maker@users.noreply.github.com>
2023-10-29 02:52:00 +0600

    cookies get
    cookies get
     যে সব value  key pair আকারে থাকে সে সবকে get করব।
     Examples 1. headers 2.cookies 3.formData

--- src/app/api/trending/route.js
+++ src/app/api/trending/route.js
@@ -10,8 +10,18 @@ import { headers } from 'next/headers'
 // যে সব value  key pair আকারে থাকে সে সবকে get করব।
 // Examples 1. headers 2.cookies 3.formData
 
+// export async function GET(req,res){
+//     const headersList = headers()
+//    const apiKey = headersList.get('api-key');
+//    return NextResponse.json({mas:apiKey})
+// }
+
+// cookies Request
+// যে সব value  key pair আকারে থাকে সে সবকে get করব।
+// Examples 1. headers 2.cookies 3.formData
+
 export async function GET(req,res){
-    const headersList = headers()
-   const apiKey = headersList.get('api-key');
-   return NextResponse.json({mas:apiKey})
+    // const token = req.cookies.get('token');
+    const token = req.cookies.get('token').name;
+    return NextResponse.json({mes:token});
 }
